f92d13452e6b0e42de63e16b9211f474bce84d80
vilyapilya <13.veter@gmail.com>
2019-10-04 17:35:18 -0700

    rename the file

---
+++ lib/logdna/logdna-rails.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# require 'logdna'
+#
+# module Logdna
+#     class RailsLogger < Logdna::Ruby
+#         include Logdna
+#         attr_accessor :api_key, :opts
+#
+#         def initialize(api_key, opts={})
+#             super(api_key, opts)
+#         end
+#     end
+# end
+#
+
+module Vilya
+  def hi
+    puts "llllllll"
+  end
+end
