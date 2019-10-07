a11a4d6738f11882ce477f9995ebc317f3820ce5
vilyapilya <13.veter@gmail.com>
2019-10-07 15:46:23 -0700

    fix so it finds the gem

--- lib/logdna-rails.rb
+++ lib/logdna-rails.rb
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 #!/usr/bin/env ruby
+=======
+# !/usr/bin/env ruby
+>>>>>>> fix so it finds the gem
 # encoding: utf-8
 require 'logdna'
 
 module Logdna
+<<<<<<< HEAD
     class RailsLogger < Logdna::Ruby
+=======
+    class Stone < Logdna::Ruby
+>>>>>>> fix so it finds the gem
         include Logdna
         attr_accessor :api_key, :opts
 
