bbcfa6147da330cf895b1e86a4591b6139b1c87c
vilyapilya <13.veter@gmail.com>
2019-10-09 11:19:46 -0700

    change the name

--- lib/logdna-rails.rb
+++ lib/logdna-rails.rb
@@ -16,8 +16,8 @@ module Logdna
         attr_accessor :api_key, :opts
 
         def initialize(api_key, opts={})
-            if(!opts[:rails_logdna_v])
-              opts[:rails_logdna_v] = "logdna-rails/#{LogDNA::VERSION}"
+            if(!opts["user-agent"])
+              opts["user-agent"] = "logdna-rails/#{LogDNA::VERSION}"
             end
             super(api_key, opts)
         end
