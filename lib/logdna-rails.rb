b2e95dc390154bc1fd1773a5939d3fb83bbb13d1
vilyapilya <13.veter@gmail.com>
2019-10-21 18:10:29 -0700

    aaa

--- lib/logdna-rails.rb
+++ lib/logdna-rails.rb
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 #!/usr/bin/env ruby
-=======
-# !/usr/bin/env ruby
->>>>>>> fix so it finds the gem
 # encoding: utf-8
 require 'logdna'
 
 module Logdna
-<<<<<<< HEAD
     class RailsLogger < Logdna::Ruby
-=======
-    class Stone < Logdna::Ruby
->>>>>>> fix so it finds the gem
         include Logdna
         attr_accessor :api_key, :opts
 
         def initialize(api_key, opts={})
-            if(!opts[:"user-agent"])
-              opts[:"user-agent"] = "logdna-rails/#{LogDNA::VERSION}"
-            end
             super(api_key, opts)
         end
     end
